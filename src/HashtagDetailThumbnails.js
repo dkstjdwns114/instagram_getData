a0174b051e7dc79a794817f4a1e1298cf6ff1f16
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-29 01:35:07 +0900

    HashTag Data Thumbnails

---
+++ src/HashtagDetailThumbnails.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const imageSize = {
+  width: "293px",
+  height: "293px"
+};
+
+export default function HashtagDetailThumbnails({
+  username,
+  imgArr,
+  shortcode,
+  totalPostCnt
+}) {
+  let thumbnailList = [];
+
+  imgArr.map((thumbnailImage, index) => {
+    (index + 1) % 3 === 0
+      ? thumbnailList.push(
+          <a
+            key={shortcode[index]}
+            href={"https://www.instagram.com/p/" + shortcode[index]}
+          >
+            <img
+              src={thumbnailImage}
+              style={imageSize}
+              alt={`${index + 1}번 이미지`}
+            />
+            <br />
+          </a>
+        )
+      : thumbnailList.push(
+          <>
+            <a
+              key={shortcode[index]}
+              href={"https://www.instagram.com/p/" + shortcode[index]}
+            >
+              <img
+                src={thumbnailImage}
+                style={imageSize}
+                alt={`${index + 1}번 이미지`}
+              />
+            </a>
+            <span> </span>
+          </>
+        );
+  });
+  return (
+    <div>
+      <p>받아온 게시물 : {shortcode.length}개</p>
+      {thumbnailList}
+    </div>
+  );
+}
